72c6fb19e706429681d1c318de2ff99503623fd9
bryhljaev <bryhljaev@korall.ru>
2018-02-21 12:40:41 +0300

    Именование бинов и старт контекста - 3 - The Basics of Spring Framework
    https://youtu.be/y-obHCFTbZ4?list=PL6jg6AGdCNaWF-sUH2QDudBRXo54zuN1t

--- SpringCore/src/main/java/com/yet/spring/core/App.java
+++ SpringCore/src/main/java/com/yet/spring/core/App.java
@@ -1,18 +1,25 @@
 package com.yet.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     public static void main(String[] args) {
-        App app = new App();
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 
-        app.client = new Client(1, "Петр Иваныч");
-        app.eventLogger = new ConsoleEventLogger();
-
+        App app = ctx.getBean(App.class);
         app.logEvent("Некоё событие для пользователя 1.");
+        app.logEvent("Некоё событие для пользователя 2.");
     }
 
-    private Client client;
-    private ConsoleEventLogger eventLogger;
+    private final Client client;
+    private final EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
 
     private void logEvent(String msg) {
         String message = msg.replaceAll(String.valueOf(client.getId()), client.getFullName());
